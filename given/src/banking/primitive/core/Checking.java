f92c46c0fc7ffc9196afb5bad6980dd083f24ac9
Ryan Schultz <reschul1@asu.edu>
2016-02-04 19:51:30 -0500

    Bug 44 Fix

--- given/src/banking/primitive/core/Checking.java
+++ given/src/banking/primitive/core/Checking.java
@@ -47,6 +47,15 @@ public class Checking extends Account {
 				if (balance < 0.0f) {
 					setState(State.OVERDRAWN);
 				}
+				// RS: Bug-44 fix
+				if (balance < -100) {
+					balance += amount;
+					if (numWithdraws > 10) {
+						balance += 2.0f;
+					}
+					numWithdraws--;
+					return false;
+				}
 				return true;
 			}
 		}
